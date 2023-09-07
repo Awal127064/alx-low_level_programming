b733970c18479487727304794aeceb7746d2abf9
Awal127964 <awalm35@gmail.com>
2023-09-07 23:36:19 +0000

    print with put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#inlude <stdio.h>
+
+/**
+ * main - A function that print using PUTS
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
