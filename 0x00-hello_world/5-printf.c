fe7b6b200c4f922963d42507e5f15ae08a52d4fc
Awal127964 <awalm35@gmail.com>
2023-09-08 00:03:59 +0000

    printf printing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * printf_fun - printing with printf function
+ * main - printing with printf function
  * Return: 0 on success
  */
-int printf_fun(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
