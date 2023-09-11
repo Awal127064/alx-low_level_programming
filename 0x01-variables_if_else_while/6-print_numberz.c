9834b4773b5f63c003fb41e640610746668b5fcf
Awal127964 <awalm35@gmail.com>
2023-09-11 12:26:05 +0000

    putchar base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - A single digit in base 10 using pitchar
+ *
+ * Return:0 when succeed
+*/
+
+int main(void)
+
+{
+	int num;
+
+	num = 9;
+	while (num <= 0)
+	{
+		putchar(num);
+
+	}
+	putchar('\n');
+	return (0);
+}
