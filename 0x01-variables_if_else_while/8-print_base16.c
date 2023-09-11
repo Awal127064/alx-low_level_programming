0ab5895aaec68c7212d27eab63afeffc8137cf38
Awal127964 <awalm35@gmail.com>
2023-09-11 22:57:43 +0000

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - hexadecimal printing
+ * Return: 0 on success
+ */
+
+
+int main(void)
+{
+	char hex_digit = '0';
+
+	while (hex_digit <= 'f') {
+
+	if ((hex_digit >= '0' && hex_digit <= '9') || (hex_digit >= 'a' && hex_digit <= 'f')) {
+		putchar(hex_digit);
+	}
+ 
+	hex_digit++;
+
+	}
+
+
+	putchar('\n');
+	return (0);
+}
+
