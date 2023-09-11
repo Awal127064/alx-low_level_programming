ea80e2f42e9756b54565eeb4ec6a9d4c122e29f0
Awal127964 <awalm35@gmail.com>
2023-09-11 23:04:06 +0000

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,15 @@ int main(void)
 {
 	char hex_digit = '0';
 
-	while (hex_digit <= 'f') {
+	while (hex_digit <= 'f')
+	{
 
-	if ((hex_digit >= '0' && hex_digit <= '9') || (hex_digit >= 'a' && hex_digit <= 'f')) {
-		putchar(hex_digit);
-	}
- 
-	hex_digit++;
+		if ((hex_digit >= '0' && hex_digit <= '9') || (hex_digit >= 'a' && hex_digit <= 'f'))
+
+		{
+			putchar(hex_digit);
+		}
+		hex_digit++;
 
 	}
 
