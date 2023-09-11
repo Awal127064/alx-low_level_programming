ce871aa18d742ab08e391d29ba60f1a977d8ef06
Awal127964 <awalm35@gmail.com>
2023-09-11 15:14:48 +0000

    numbers seperated by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - putchar numbers seperated by ,
+ * Return: 0 on success
+ */
+
+int main(void)
+
+{
+	int num;
+	num = '0';
+	while (num <= '9')
+	{
+		putchar(num);
+
+		if (num < '9')
+	{
+			putchar(',');
+			putchar(' ');
+	}
+
+		num++;
+
+	}
+
+	putchar('\n');
+	return (0);
+}
