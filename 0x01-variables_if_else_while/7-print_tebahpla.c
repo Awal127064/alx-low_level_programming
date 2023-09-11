135046bcac80e4ac7e3d397226cccc694a5da523
Awal127964 <awalm35@gmail.com>
2023-09-11 13:49:31 +0000

    Reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - A function to pitchar reverse alphabet
+ * Return: o when right
+ */
+
+int main(void)
+
+{
+	int alp;
+
+	alp = 'z';
+	while (alp >= 'a')
+	{
+		putchar(alp);
+		alp--;
+	}
+	putchar('\n');
+	return ('0');
+}	
